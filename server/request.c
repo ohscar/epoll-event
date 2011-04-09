553030cdba2130d76ac0ee78f3eff604c9b2556e
overred <overred@gmail.com>
2011-04-10 00:29:07 +0800

    state table const

--- server/request.c
+++ server/request.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include "debug.h"
 #include "request.h"
-#define MAXLINE 1024*10
+#define MAXLINE 1024
 
 
 
@@ -26,40 +26,32 @@ void
 request_parse(request *req)
 {
 
-	_table['G']=CMD;
-	_table['E']=CMD;
-	_table['T']=CMD;
-	int ct;
-	for(ct=48;ct<58;ct++)
-		_table[ct]=LEN;
-	_table['\r']=STP;
-	_table['\n']=STP;
-	
 	if(req->data!=NULL)
 	{
-		char ch=req->data[0];
-		int len=strlen(req->data);
+		unsigned char ch=0;
 		char *data=req->data;
 		int idx=0,pos=0;
-		char buf[1024]={0};
-		states pre_state=NO;
+		char line[MAXLINE]={0};
+		states pre_state=BODY;
 		int goon=1;
-		while(data[idx]!='\0'&&goon==1)
+		while(data[idx]!='\0')
 		{
-	  	 char ch=data[idx];
+		 if(goon==0)break;
+
+		 ch=(unsigned)data[idx];
 	 	 switch(_table[ch])
 	 	 {
 			case CMD:
 				{
 					INFO("CMD");
-					buf[pos++]=data[idx++];
+					line[pos++]=data[idx++];
 					pre_state=_table[ch];
 					break;
 				}
 			case LEN:
 				{
 					INFO("LEN");
-					buf[pos++]=data[idx++];
+					line[pos++]=data[idx++];
 					pre_state=_table[ch];
 
 					break;
@@ -67,30 +59,33 @@ request_parse(request *req)
 			case STP:
 				{
 					INFO("STP");
-					buf[pos]='\0';
+					line[pos]='\0';
 					switch(pre_state)
 					{
 						case CMD:
-							req->cmd=buf;
-							LOG("cmd:%s",buf);
+							req->cmd=line;
+							LOG("cmd:%s",line);
 							break;
 						case LEN:
-							req->length=atoi(buf);
-							LOG("len:%s",buf);
+							req->length=atoi(line);
+							LOG("len:%s",line);
 							break;
+						default:break;
 
 					}
-					buf[0]='\0';
+					line[0]='\0';
 					pos=0;
 					idx+=2;
 					break;
 				}
-			case NO:
+			case BODY:
 				{
-					while(pos<req->length&&data[idx]!='\0')
-						buf[pos++]=data[idx++];
-					buf[pos]='\0';
-					req->body=buf;
+					INFO("body");
+					while(pos<req->length&&data[idx]!='\0'&&pos<MAXLINE)
+						line[pos++]=data[idx++];
+					line[pos]='\0';
+					req->body=line;
+					goon=0;
 					break;
 				}
 			 default:
